ab084ecb241c369c5b2daf50183aee93ee2b769f
Amaan <147125326+randomguy169@users.noreply.github.com>
2024-04-12 18:43:38 +0000

    day1

--- day1.js
+++ day1.js
@@ -2,10 +2,11 @@ const findLongest = (str) => {
   let words = str.split(" ");
   words = words.sort((a, b) => b.length - a.length);
   const longestWord = words[0];
+  ayaanIsNoob = true;
   return longestWord;
 };
 console.log(
   findLongest(
-    "Ayaan is the only person in the entire world who was born without a brain"
+    "Ayaan is the only creature in the entire world who was born without a brain"
   )
 );
